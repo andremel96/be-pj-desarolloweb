a0bf3d77bb2a3cf239e0d9226f3aa99699b17814
Andrea Melgar <andimelgar96@gmail.com>
2021-11-02 00:21:10 -0600

    test

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 routes(app)
-const PORT=process.env.PORT || 3000;
+const PORT=process.env.PORT || 3001;
 
 const server = app.listen(PORT, () => {
     console.log(`
